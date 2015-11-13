ab5092fc6ca062cde952efa1040a3c37b457f62d
SimonGonzalezCepeda <simongonzalez@iesebre.com>
2015-11-13 17:08:28 +0100

    Added code to make the ArrayList.

---
+++ app/src/main/java/com/iesebre/dam2/simon/todo/TodoArrayList.java
@@ -0,0 +1,12 @@
+package com.iesebre.dam2.simon.todo;
+
+import java.util.ArrayList;
+
+/**
+ * Created by sylver on 13/11/15.
+ */
+public class TodoArrayList extends ArrayList<TodoItem> {
+
+
+
+}
